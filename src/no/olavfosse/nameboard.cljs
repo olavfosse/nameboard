5a1f8c8e760a8b6ef7ac4b25f64db419f2a118bf
Olav Fosse <post@olavfosse.no>
2024-09-01 19:27:27 +0200

    tldraw running!

--- src/no/olavfosse/nameboard.cljs
+++ src/no/olavfosse/nameboard.cljs
@@ -1,3 +1,10 @@
-(ns no.olavfosse.nameboard)
+(ns no.olavfosse.nameboard
+  (:require
+    ["react-dom" :as ReactDOM]
+    ["react" :as React]
+    ["tldraw" :refer [Tldraw]]))
 
-(defn init! [])
+(defn init! []
+  (ReactDOM/render
+    (React/createElement Tldraw nil)
+    (.getElementById js/document "root")))
